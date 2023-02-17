825d92c58c06b23d3c2373f2fea4d835020b0518
Gail Hernandez <gailgail@metronome.com>
2023-02-16 22:17:43 -0800

    Add team setup

--- unit1/MarsWeek/src/MarsExpedition.java
+++ unit1/MarsWeek/src/MarsExpedition.java
@@ -3,8 +3,31 @@ package unit1.MarsWeek.src;
 import java.util.Scanner;
 
 public class MarsExpedition {
-  public MarsExpedition() {
+  // public MarsExpedition() {
+    public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.println("Booting up...");
+    System.out.println("What's your name?");
+    String name = scan.nextLine();
+    System.out.println("Hi, " + name + "! Welcome to the expedition prep program. Are you ready to head out into the new world? Type Y or N.");
+    String userReady = scan.nextLine();
+
+    if (userReady.equalsIgnoreCase("y")) {
+      System.out.println("I knew you'd say that. You're a team leader for a reason.");
+    } else {
+      System.out.println("Too bad. You're a team leader. You have to go.");
+    }
+
+    System.out.println("How many people do you want on your team?");
+    int teammates = scan.nextInt();
+
+    if (teammates > 2) {
+      System.out.println("That's way too many people. We can only send 2 more members.");
+      teammates = 2;
+    } else if (teammates < 2) {
+      System.out.println("That's not enough people. We'll send you 2 more members.");
+    } else {
+      System.out.println("Great!");
+    }
   }
 }
