10cc2dc593d6976c2b90e2d5fce51e610e1fbbbd
Gail Hernandez <gailgail@metronome.com>
2023-02-16 21:29:09 -0800

    Add name

--- unit1/MarsWeek/src/GuessingGame.java
+++ unit1/MarsWeek/src/GuessingGame.java
@@ -1,7 +1,11 @@
 package unit1.MarsWeek.src;
 
+import java.util.Scanner;
+
 public class GuessingGame {
   public static void main(String[] args) {
-    System.out.println("Hi! What's your name?");
+    System.out.println("Hi! What's your name? ");
+    Scanner scan = new Scanner(System.in);
+    String name = scan.nextLine();
   }
 }
