9dc8590a7b5c415deff8784ae36ae3f14f0f58a0
Gail Hernandez <gailgail@metronome.com>
2023-02-16 22:09:14 -0800

    Add part 4 expedition bootup

---
+++ unit1/MarsWeek/src/MarsExpedition.java
@@ -0,0 +1,10 @@
+package unit1.MarsWeek.src;
+
+import java.util.Scanner;
+
+public class MarsExpedition {
+  public MarsExpedition() {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Booting up...");
+  }
+}
