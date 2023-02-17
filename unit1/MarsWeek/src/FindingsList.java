d0d795082ea33c16a35458121703c55f141f31c1
Gail Hernandez <gailgail@metronome.com>
2023-02-16 22:49:38 -0800

    Add fossils

--- unit1/MarsWeek/src/FindingsList.java
+++ unit1/MarsWeek/src/FindingsList.java
@@ -1,11 +1,14 @@
 package unit1.MarsWeek.src;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class FindingsList {
   public FindingsList() throws InterruptedException {
     Thread.sleep(500);
     System.out.println("Welcome back!");
+
     ArrayList<String> rocks = new ArrayList<String>();
     rocks.add("rock");
     rocks.add("weird rock");
@@ -20,5 +23,27 @@ public class FindingsList {
 
     System.out.print("Here's the new list: ");
     System.out.println(rocks);
+
+    System.out.println("Downloading fossil data...");
+    Thread.sleep(1000);
+
+    HashMap<String, String> fossils = new HashMap<>();
+    fossils.put("bird fossil", "the fossil has wings, implying it was once capable of flight");
+    fossils.put("fish fossil", "the fossil is vaguely fish-shaped, implying there was once water");
+    fossils.put("tooth fossil", "the tooth from an unknown fossil");
+    System.out.println("Fossil data downloaded.");
+
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Which fossil do you want to learn more about? Enter bird, fish, or tooth.");
+    String fossilChoice = scan.nextLine();
+
+    if (fossilChoice.equalsIgnoreCase("bird") || fossilChoice.equalsIgnoreCase("fish")
+        || fossilChoice.equalsIgnoreCase("tooth")) {
+      System.out.println(
+          "Here's some information about the " + fossilChoice + " fossil: "
+              + fossils.get(fossilChoice.toLowerCase() + " fossil"));
+    } else {
+      System.out.println("Sorry, I don't know about that.");
+    }
   }
 }
